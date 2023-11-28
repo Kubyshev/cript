8e7466948ae3016b4d3caa4e21d98e6d8c3744a9
Kubyshev <117076814+Kubyshev@users.noreply.github.com>
2023-11-28 13:19:54 +0300

    Add files via upload

---
+++ 1/1.1.py
@@ -0,0 +1,26 @@
+from itertools import repeat
+
+a = input("Введите шифруемую строку ")
+
+def cypher(string, column):
+    l = list()
+    for i in range(column):
+        l.append(string[i::column])
+    return "".join(l)
+
+def decypher(string, column):
+    l = list(repeat("",len(string)//column+1))
+    i = 0
+    cnt = 0
+    last_line_len = len(string)%column
+    for c in string:
+        if i >= (len(string)//column+(0 if cnt>=last_line_len else 1)):
+            i = 0
+            cnt+=1
+        l[i]+=c
+        i+=1
+    return "".join(l)
+
+a1 = cypher(a,6)
+a2 = decypher(a1,6)
+print("Зашифрованная строка {a1}\nРазшифрованная строка {a2}".format(a1=a1, a2=a2))
---
+++ 1/1.3..py
@@ -0,0 +1,26 @@
+MIN = 32
+MAX = 127
+
+RAND_MAX = 20
+
+from random import randint
+
+
+def shift(ch, shift_num):
+    if shift_num < 0:
+        return shift(ch, MAX - MIN + shift_num)
+    return chr((ord(ch) - MIN + shift_num) % (MAX - MIN) + MIN)
+
+
+def cypher(string, shift_num):
+    return [ord(shift(x, shift_num)) + (MAX - MIN) * randint(1, RAND_MAX) for x in string]
+
+
+def decypher(l, shift_num):
+    return "".join([shift(chr(x % (MAX - MIN)), -shift_num) for x in l])
+
+
+a = input("Введите шифруемую строку ")
+a1 = cypher(a, 6)
+a2 = decypher(a1, 6)
+print("Зашифрованная строка {a1}\nРазшифрованная строка {a2}".format(a1=a1, a2=a2))
